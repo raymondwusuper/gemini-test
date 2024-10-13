277ca4d21cc350a80668ad500a478849491e213b
raymondwusuper <raymond.wu.super@gmail.com>
2024-10-12 23:38:10 -0700

    testing gemini api

---
+++ testgemini.py
@@ -0,0 +1,20 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key="AIzaSyBTE2Oh74mhQcYR0KZPnn-7VsEqEmXFCwQ")
+model = genai.GenerativeModel(model_name="gemini-1.5-flash", system_instruction="You are a therapist, helping people who are mentally ill. Generate outputs to questions by giving them a task list to improve their situation.")
+chat_session = model.start_chat(history=[])
+
+def generate_content(user_input):
+    print()
+    response = chat_session.send_message(user_input)
+    model_response = response.text
+    print(model_response)
+    print()
+    chat_session.history.append({"role": "user", "parts": [user_input]})
+    chat_session.history.append({"role": "model", "parts": [model_response]})
+
+while True:
+    uinput = input("You: ") #ADD CALL FROM WEBSITE
+    generate_content(uinput)
